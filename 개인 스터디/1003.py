9b4b7fc9eb712f016884ffabd093d705bbaf2036
SidneyKim <wz0627@gmail.com>
2022-04-28 15:45:30 +0900

    사칙연산 부분 완료

---
+++ 개인 스터디/1003.py
@@ -0,0 +1,54 @@
+"""int fibonacci(int n) {
+    if (n == 0) {
+        printf("0");
+        return 0;
+    } else if (n == 1) {
+        printf("1");
+        return 1;
+    } else {
+        return fibonacci(n‐1) + fibonacci(n‐2);
+    }
+}"""
+"""fibonacci(3)을 호출하면 다음과 같은 일이 일어난다.
+
+fibonacci(3)은 fibonacci(2)와 fibonacci(1) (첫 번째 호출)을 호출한다.
+fibonacci(2)는 fibonacci(1) (두 번째 호출)과 fibonacci(0)을 호출한다.
+두 번째 호출한 fibonacci(1)은 1을 출력하고 1을 리턴한다.
+fibonacci(0)은 0을 출력하고, 0을 리턴한다.
+fibonacci(2)는 fibonacci(1)과 fibonacci(0)의 결과를 얻고, 1을 리턴한다.
+첫 번째 호출한 fibonacci(1)은 1을 출력하고, 1을 리턴한다.
+fibonacci(3)은 fibonacci(2)와 fibonacci(1)의 결과를 얻고, 2를 리턴한다.
+1은 2번 출력되고, 0은 1번 출력된다. N이 주어졌을 때, fibonacci(N)을 호출했을 때, 0과 1이 각각 몇 번 출력되는지 구하는 프로그램을 작성하시오.
+
+"""
+"""첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+각 테스트 케이스는 한 줄로 이루어져 있고, N이 주어진다. N은 40보다 작거나 같은 자연수 또는 0이다.
+각 테스트 케이스마다 0이 출력되는 횟수와 1이 출력되는 횟수를 공백으로 구분해서 출력한다.
+"""
+
+t = int(input())
+ns = map(int, [input() for _ in range(t)])
+
+cnt0 = 0
+cnt1 = 0
+
+def fibonacci( n):
+    global cnt0, cnt1
+    if (n == 0):
+        print("0")
+        cnt0 += 1
+        return 0
+    
+    elif (n == 1):
+        print("1")
+        cnt1 += 1
+        return 1
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+for i in ns:
+    cnt0 = 0
+    cnt1 = 0
+    fibonacci(i)
+    print(cnt0, " ", cnt1)
+
