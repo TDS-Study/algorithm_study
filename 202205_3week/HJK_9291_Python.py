5e0de502970cd9ca63214b56acde764e6467ddb2
SidneyKim <wz0627@gmail.com>
2022-05-09 21:32:03 +0900

    9291완료

--- 202205_3week/HJK_9291_Python.py
+++ 202205_3week/HJK_9291_Python.py
@@ -16,61 +16,73 @@
 출력
 각 테스트 케이스에 걸쳐 "Case x:"를 출력한 후, 공백 한 칸 뒤에 풀이가 올바르면 "CORRECT"를, 아니면 "INCORRECT"를 출력한다. x는 테스트 케이스 번호이며, 1부터 시작한다."""
 
+# 반복할 횟수를 입력받는다
 t = int(input())
+# 결과 출력을 저장 할 변수
 result = []
 
-for i in range(t):
-    # 수평 저장 리스트
+# 리스트를 넘겨받아 중복숫자가 있으면 False, 없으면 True 반환
+def checkList(l):
+    r = True
+
+    for i in l:
+        # Set 자료구조를 사용, 1개 행에 유일한 아이템이 9개가 아니면 틀렸다
+        if len(set(i)) != 9:
+            r = False
+            break
+    
+    return r
+
+def getInput():
+    # 수평 값(행)) 저장 리스트
     hl = []
-    # 수직값 저장 리스트, 9개의 리스트를 가진 이중배열
+    # 수직 값(열)) 저장 리스트 초기화
     vl = [list() for _ in range(9)]
+    # 3x3 스퀘어 저장 리스트 초기화
     sql = [list() for _ in range(9)]
 
-    # 9 행을 읽는다
-    for j in range(9):
+    for i in range(9):
         l = input().split()
+        # 수평 
         hl.append(l)
 
         for h in range(len(l)):
+            # 각행의 수직 값 저장
             vl[h].append(l[h])
 
-print (hl)    
+    # 3x3 스퀘어 리스트
+    rowNum = 0
+    # x 위치 시작점
+    for x in [0,3,6]:
+        # y 위치 시작점
+        for y in [0,3,6]:
+            
+            for x1 in [0,1,2]:
+                for y1 in [0,1,2]:
+                    # 시작점 x,y 를 기준으로 3x3
+                    sql[rowNum].append(hl[x+x1][y+y1])
+
+            # 행번호는 0~8
+            rowNum += 1   
+    
+    return hl, vl, sql
     
 for i in range(t):
 
-    for j in hl:
-        # 1행에 유일한 아이템이 9개가 아니면 틀렸다
-        if len(set(j)) != 9:
-            result = False
-            break
-    
-    # 행검사 후 결과
-    if result == False:
-        print(f'Case {i+1}: INCORRECT')
-        #continue
-    
-    for j in vl:
-        if len(set(j)) != 9:
-            result = False
-            break
-    
-    # 열검사 후 결과
-    if result == False:
-        print(f'Case {i+1}: INCORRECT')
-        #continue
+    h, v, sq = getInput()
 
-    for j in [0,3,6]:
-        for h in [0,1,2]:
-            for k in [0,1,2]:
-                for l in [0,3,6]:
-                    print("row:",j+h)
-                    print("row" , k+j)
-                    
-                    sql[j+h].append(hl[k+l][k+l])
-                
-                
+    # 3개의 리스트 중 1개라도 틀린것이 있으면 틀림
+    if checkList(h) == False or checkList(v) == False or checkList(sq) == False:
+        result.append(f'Case {i+1}: INCORRECT')        
+    else:
+        result.append(f'Case {i+1}: CORRECT')
 
-print(sql)
+    # 한줄 건너 뜀
+    if i != (t-1):
+        input()
+
+# 리스트 항목을 줄바꿈으로 분리하여 출력한다
+print(*result, sep='\n')
 
 
 
