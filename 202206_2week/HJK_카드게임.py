0ad84d180bf7d751aacaf5f15087437988d1d6b7
SidneyKim <wz0627@gmail.com>
2022-06-04 22:10:29 +0900

    카드게임 완료

--- 202206_2week/HJK_카드게임.py
+++ 202206_2week/HJK_카드게임.py
@@ -17,19 +17,64 @@ def getInput():
     global iRound, cards1, cards2
 
     # 라운드(1~100)를 랜덤하게 생성 
-    iRound = random.randint(1, 100)
+    iRound = random.randint(1, 3)
 
     for i in range(iRound):
         # 1~52 까지 숫자를 랜덤하게 5개 뽑고 라운드수 만큼 추가
-        cards1.append([random.randrange(1,52,1) for j in range(5)])
-        cards2.append([random.randrange(1,52,1) for j in range(5)])
+        a = [random.randrange(1,52,1) for j in range(5)]
+        # 검증하기 쉽도록 정렬해준다
+        a.sort()
+        cards1.append(a)
+
+        b = [random.randrange(1,52,1) for j in range(5)]
+        b.sort()
+        cards2.append(b)
+    
+    print(cards1, '\n', cards2)
 
 # 입력을 받는다
 getInput()
 
-prevRound 
+def rule1(card1 = [], card2 = []):
+    # 두 덱의 유일한 숫자가 10개이면 통과
+    uniqueCount = len(set(card1 + card2))
+    print(f"Check1 Unique Count: {uniqueCount}")
+
+    if uniqueCount == 10:
+        return True
+    else:        
+        return False
+
+def rule2(prevCards = [], cards = []):
+    # 두 덱의 
+    if len(prevCards) == 0:
+        print("no prev cards")
+        return True
+    else:
+        uniqueCount = len(set(prevCards + cards))
+        print(f"Check2 Unique Count: {uniqueCount}")
+
+        if uniqueCount > 8:
+            return True
+        else:
+            return False
+
+prevRound1, prevRound2 = [], []
+failCnt = 0
 
 # 라운드 별로 체크한다
 for i in range(iRound):
+
+    if rule1(cards1[i], cards2[i]) and \
+        rule2(cards1[i], prevRound1) and \
+        rule2(cards2[i], prevRound2):
+        pass
+    else:
+        failCnt += 1
+    
+    prevRound1 = cards1[i]
+    prevRound2 = cards2[i]
+
+print(failCnt)
     
 
