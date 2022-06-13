3de3cc901f8b3f746f83d7a1ccbd38865688f8d8
ho910728@gmail.com <ho910728@gmail.com>
2022-06-13 13:33:27 +0900

    과자나눠주기 체크 함수 내 while문 제거 ( '//' 연산자로 대체)

--- 202206_3week/SHJ_16401_과자나눠주기_실버2.py
+++ 202206_3week/SHJ_16401_과자나눠주기_실버2.py
@@ -1,8 +1,3 @@
-
-
-from re import I
-
-
 n, c = map(int,input().split())                 # nephew 조카, cookie
 l = list(map(int, input().split())) # length
 
@@ -13,20 +8,18 @@ e = max(l)
 def isOk(mid:int):
     cnt = 0 # mid로 가능한 과자의 개수
     for length in l:
-        i =1
-        if length < mid:
-            continue
+        l_cnt = length // mid
+        if l_cnt < 1:    # 내림차순 정렬을 했기 때문에 length가 mid보다 작기 시작하면 나머지 요소는 다 작다고 봐야함.
+            break
         else:
-            while length >= mid*i:
-                cnt += 1
-                i += 1
-        
-        if cnt >= n:
+            cnt += l_cnt # mid 기준 몇개로 나눌 수 있는지 판단.
+    
+        if cnt >= n:     # 과자 개수가 조카 인원수를 충족하면 for문을 멈추고 True 리턴
             return True
     
-    if cnt < n:
+    if cnt < n:         # 과자 개수가 부족하면 False
         return False
-    else:
+    else:               # 과자 개수가 충분하면 True
         return True
 
 if sum(l) < n:      # 모든 과자를 합쳐도 조카 수 보다 작은 경우
