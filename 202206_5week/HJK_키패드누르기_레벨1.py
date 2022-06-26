18e17b6e6b4c719142c1f5b67e1dbbe258d2cdac
SidneyKim <wz0627@gmail.com>
2022-06-26 17:36:06 +0900

    푸는중

--- 202206_5week/HJK_키패드누르기_레벨1.py
+++ 202206_5week/HJK_키패드누르기_레벨1.py
@@ -1,15 +1,48 @@
+def get_dist(p_a:int, p_b:int):
+    xlist = {"*": 1, "#": 3, 0:2, 1:1, 2:2, 3:3, 4:1, 5:2, 6:3, 7:1, 8:2, 9:3}
+    ylist = {"*": 1, "#": 1, 0:1, 1:4, 2:4, 3:4, 4:3, 5:3, 6:3, 7:2, 8:2, 9:2}
+
+    d1 = xlist[p_a] - xlist[p_b]
+    d2 = ylist[p_a] - ylist[p_b]
+
+    return abs(d1) + abs(d2)
+
 def solution(numbers, hand):
     answer = ''
-    hand = ''
+    usingHand = ''
+    l_position = 0
+    r_position = 0
+    
     for i in numbers:
         if i in [1,4,7]:
-            hand = 'left'
+            usingHand = 'L'
+            l_position = i
         elif i in [3,6,9]:
-            hand = 'right'
+            usingHand = 'R'
+            r_position = i
         else:
-            hand = 'left'
+            dist_from_left = get_dist(i, l_position)
+            dist_from_right = get_dist(i, r_position)
 
-        answer += hand
+            if dist_from_left < dist_from_right:
+                
+
+                usingHand = 'L'
+                l_position = i
+              
+            elif dist_from_left > dist_from_right:
+                usingHand = 'R'
+                r_position = i
+              
+            else:
+                if hand == 'left':
+                    usingHand = 'L'
+                    l_position = i
+                else:
+                    usingHand = 'R'
+                    r_position = i
+            
+        answer += usingHand
 
     return answer
 
