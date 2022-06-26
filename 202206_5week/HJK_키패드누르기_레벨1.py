101e8869bac629dd18e05596cd419c80061bab97
Sidney Kim <wz0627@gmail.com>
2022-06-26 17:54:14 +0900

    제출완료

--- 202206_5week/HJK_키패드누르기_레벨1.py
+++ 202206_5week/HJK_키패드누르기_레벨1.py
@@ -1,40 +1,43 @@
-def get_dist(p_a:int, p_b:int):
-    xlist = {"*": 1, "#": 3, 0:2, 1:1, 2:2, 3:3, 4:1, 5:2, 6:3, 7:1, 8:2, 9:3}
+def get_dist(a:int, b:int):
+    # 숫자별 x, y 좌표를 딕셔너리로 저장
+    xlist = {"*": 1, "#": 3, 0:2, 1:1, 2:2, 3:3, 4:1, 5:2, 6:3, 7:1, 8:2, 9:3}    
     ylist = {"*": 1, "#": 1, 0:1, 1:4, 2:4, 3:4, 4:3, 5:3, 6:3, 7:2, 8:2, 9:2}
-
-    d1 = xlist[p_a] - xlist[p_b]
-    d2 = ylist[p_a] - ylist[p_b]
-
+    
+    # x 좌표거리, y좌표거리 구함
+    d1 = xlist[a] - xlist[b]
+    d2 = ylist[a] - ylist[b]
+    
+    # 두 지점간 거리 반환
     return abs(d1) + abs(d2)
 
 def solution(numbers, hand):
     answer = ''
     usingHand = ''
-    l_position = 0
-    r_position = 0
+    l_position = '*'
+    r_position = '#'
     
     for i in numbers:
+        # 규칙1, 1,4,7은 왼손이 누른다
         if i in [1,4,7]:
             usingHand = 'L'
             l_position = i
+        # 규칙2, 3,6,9는 오른손이 누른다
         elif i in [3,6,9]:
             usingHand = 'R'
             r_position = i
         else:
+            # 거리를 구해서 비교한다
             dist_from_left = get_dist(i, l_position)
             dist_from_right = get_dist(i, r_position)
 
             if dist_from_left < dist_from_right:
-                
-
                 usingHand = 'L'
                 l_position = i
-              
             elif dist_from_left > dist_from_right:
                 usingHand = 'R'
                 r_position = i
-              
             else:
+                # 거리가 같은 경우 왼손잡이이면 왼손
                 if hand == 'left':
                     usingHand = 'L'
                     l_position = i
@@ -47,6 +50,6 @@ def solution(numbers, hand):
     return answer
 
 
-a = solution([1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5], 'right')
+a = solution([5,8,0], 'right')
 
 print(a)
