86ef5b5a6aa79f91b47fdd66efd59bea3549c8ab
ho910728@gmail.com <ho910728@gmail.com>
2022-07-19 11:58:28 +0900

    안전영역 remove -> visit 변경: 시간초과

--- 202207_2week/SHJ_2468_안전영역_실버1.py
+++ 202207_2week/SHJ_2468_안전영역_실버1.py
@@ -21,42 +21,46 @@ class Coordi:
         self.x = x
         self.y = y
 
-def bfs(coordi):
+def bfs(coordi,visit):
     safetyZones = 0
     
-    while coordi:
+    for c in coordi:
+        if visit[c[0]][c[1]]:
+            continue
         safetyZones += 1
-        queue = deque([coordi.pop(0)])
+        queue = deque([c])
+        visit[c[0]][c[1]] = True
         while queue:
             cur =  queue.popleft()
-            
+
             # 상
-            if [cur[0], cur[1]-1] in coordi:
-                coordi.remove([cur[0], cur[1]-1])
+            if [cur[0], cur[1]-1] in coordi and visit[cur[0]][cur[1]-1] == False:
                 queue.append([cur[0], cur[1]-1])
+                visit[cur[0]][cur[1]-1] = True
             # 하
-            if [cur[0], cur[1]+1] in coordi:
-                coordi.remove([cur[0], cur[1]+1])
+            if [cur[0], cur[1]+1] in coordi and visit[cur[0]][cur[1]+1] == False:
                 queue.append([cur[0], cur[1]+1])
+                visit[cur[0]][cur[1]+1] = True
             # 좌
-            if [cur[0]-1, cur[1]] in coordi:
-                coordi.remove([cur[0]-1, cur[1]])
+            if [cur[0]-1, cur[1]] in coordi and visit[cur[0]-1][cur[1]] == False:
                 queue.append([cur[0]-1, cur[1]])
+                visit[cur[0]-1][cur[1]] = True
             # 우
-            if [cur[0]+1, cur[1]] in coordi:
-                coordi.remove([cur[0]+1, cur[1]])
+            if [cur[0]+1, cur[1]] in coordi  and visit[cur[0]+1][cur[1]] == False:
                 queue.append([cur[0]+1, cur[1]])
+                visit[cur[0]+1][cur[1]] = True
         
     return safetyZones    # words 리스트에 target은 있으나 변환할 수 없는 경우
 
 def countSafetyZone(n, h):
+    visit = [[False for i in range(n)] for j in range(n)]
     coordi = []
     for x in range(n):
         for y in range(n):
              if altitude[y][x] > h:
                 coordi.append([x,y])  # 좌표 리스트로 가야지
 
-    safetyZones = bfs(coordi)
+    safetyZones = bfs(coordi, visit)
     return safetyZones
 
 def solution(n, min, max):
@@ -67,30 +71,30 @@ def solution(n, min, max):
             answer = safetyZones
     print(answer)
 
-# n = int(input())
-# altitude = []
-# max_v = 1
-# min_v = 100
-# for i in range(n):
-#     # row = list(map(int, input().split()))
-#     row = list(map(int, sys.stdin.readline().split()))
-    
-#     # bfs 실행할 범위 찾기
-#     if(max_v < max(row)):
-#         max_v = max(row)
-#     if(min_v > min(row)):
-#         min_v = min(row)
-#     altitude.append(row)
-
-n = 5
+n = int(input())
 altitude = []
-altitude.append(list(map(int, '6 8 2 6 2'.split())))
-altitude.append(list(map(int, '3 2 3 4 6'.split())))
-altitude.append(list(map(int, '6 7 3 3 2'.split())))
-altitude.append(list(map(int, '7 2 5 3 6'.split())))
-altitude.append(list(map(int, '8 9 5 2 7'.split())))
-min_v = 2
-max_v = 9
+max_v = 1
+min_v = 100
+for i in range(n):
+    # row = list(map(int, input().split()))
+    row = list(map(int, sys.stdin.readline().split()))
+    
+    # bfs 실행할 범위 찾기
+    if(max_v < max(row)):
+        max_v = max(row)
+    if(min_v > min(row)):
+        min_v = min(row)
+    altitude.append(row)
+
+# n = 5
+# altitude = []
+# altitude.append(list(map(int, '6 8 2 6 2'.split())))
+# altitude.append(list(map(int, '3 2 3 4 6'.split())))
+# altitude.append(list(map(int, '6 7 3 3 2'.split())))
+# altitude.append(list(map(int, '7 2 5 3 6'.split())))
+# altitude.append(list(map(int, '8 9 5 2 7'.split())))
+# min_v = 2
+# max_v = 9
 
 solution(n, min_v, max_v)
 
