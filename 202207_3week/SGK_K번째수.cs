77271680797fa1d6089ae2a896c8f6479481fe53
Sun-gil, Kim <52283175+KimSun-gil@users.noreply.github.com>
2022-07-22 08:25:21 +0900

    AddFile SGK_K번째수

---
+++ 202207_3week/SGK_K번째수.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class SGK_K번째수
+    {
+        public int[] Solution(int[] array, int[,] commands)
+        {
+            List<int> answer = new List<int>();
+
+            for(int i = 0; i < commands.GetLength(0); i++)
+            {
+                int from = commands[i, 0];
+                int to = commands[i, 1];
+                int pick = commands[i, 2];
+
+                List<int> saveList = new List<int>();
+
+                for(int k = 0; k < array.Length; k++)
+                {
+                    if( k + 1  >= from && k + 1 <= to)
+                    {
+                        saveList.Add(array[k]);
+                    }
+                }
+
+                saveList.Sort();
+                answer.Add(saveList[pick - 1]);
+
+            }
+
+            return answer.ToArray();
+        }
+
+    }
+}
